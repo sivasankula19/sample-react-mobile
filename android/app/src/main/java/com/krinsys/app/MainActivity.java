bba326b8365b6fdda97ee67eb58a4c3775ee9ba0
KrinSys - Siva #1 <krinsys@KrinSyss-MacBook-Air.local>
2025-06-08 23:30:58 +0530

    Initial Template added

---
+++ android/app/src/main/java/com/krinsys/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.krinsys.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.krinsys.app',
+  appName: 'KrinSys-Mobile',
+  webDir: 'build'
+};
+
+export default config;
